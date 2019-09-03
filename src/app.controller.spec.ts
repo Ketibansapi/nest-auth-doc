001da87a42fb53055e05b128fe626c23112e7558
Deary <dearyherdiano@gmail.com>
2019-09-03 22:15:04 +0800

    git clearance 1

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -17,6 +17,8 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
     //  expect(appController.getHello()).toBe('Hello World!');
+
+    // GIT clearance
     });
   });
 });
